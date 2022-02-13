f703c4efe7103980f09d3030ec72d58f8954c0c9
Dmitry <mvrchenko@gmail.com>
2022-02-13 11:26:59 +0200

    Create part 8.js

---
+++ part 8.js
@@ -0,0 +1,7 @@
+String.prototype.toAlternatingCase = function () {
+    return [...this]
+      .map(
+        c => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase())
+      )
+      .join('')
+}
